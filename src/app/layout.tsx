f6c4971209b97c10ca65b3d55b845a0a77d4657f
kellton hamaia <hamaia.kell@gmail.com>
2024-01-17 17:21:02 -0300

    fix: fixed aboutMe.tsx title not aligned correctly with text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   }) {
   
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark ">
       <body 
         className={`${inter.className} dark:bg-noise dark:bg-fixed`}
       >
--- src/components/app/AboutMe.tsx
+++ src/components/app/AboutMe.tsx
@@ -3,10 +3,10 @@ import { Card, CardDescription, CardTitle } from "@/components/ui/card"
 export const AboutMe = () => {
     return (
         <Card className="flex flex-col gap-2 p-4 mt-5 bg-transparent border-2 border-pink-100 dark:bg-zinc-900/20 dark:border-none">
-            <CardTitle className="text-black dark:text-white"> 
+            <CardTitle className="p-2 text-black dark:text-white"> 
                 Olá! 😃 
             </CardTitle>
-            <CardDescription className="p-2 leading-relaxed text-black break-words dark:text-white md:text-md">
+            <CardDescription className="px-2 leading-relaxed text-black break-words dark:text-white md:text-md">
                 Meu nome é Kellton Hamaia, e sou desenvolvedor fullstack!
                 Atualmente estou no quarto semestre da graduação em Tecnologias em Sistemas para a Internet, pelo IFPR de Jacarezinho.
                 Amo este mundo da programação, pois através dos códigos, sou capaz de trazer a vida minhas ideias do zero :)
