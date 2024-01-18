f5aa1417d95abdaac10b759c50eb3028fd10b6f3
kellton hamaia <hamaia.kell@gmail.com>
2024-01-18 14:44:41 -0300

    feat: populated file techs.ts with all infos

--- src/data/techs.ts
+++ src/data/techs.ts
@@ -1,13 +1,160 @@
 type Techs = {
     url: string,
-    title: string,
-    description: string
+    learning?: boolean,
+    name: string,
 }
 
-export const Technologies: Techs[] = [
+export const frontend: Techs[] = [
     {
-        url: "",
-        title: "",
-        description: ""
+        url: "/brands/css.png",
+        name: "CSS",
+    },
+    {
+        url: "/brands/daisyui.png",
+        name: "DaisyUI",
+    },
+    {
+        url: "/brands/framer-motion.png",
+        name: "Framer-Motion",
+    },
+    {
+        url: "/brands/html.png",
+        name: "HTML",
+    },
+    {
+        url: "/brands/javascript.png",
+        name: "Javascript",
+    },
+    {
+        url: "/brands/nextjs.png",
+        name: "Nextjs",
+        learning: true
+    },
+    {
+        url: "/brands/radix.png",
+        name: "Radix",
+    },
+    {
+        url: "/brands/react-router.png",
+        name: "React-router",
+    },
+    {
+        url: "/brands/react.png",
+        name: "React",
+    },
+    {
+        url: "/brands/redux.png",
+        name: "Redux",
+        learning: true
+    },
+    {
+        url: "/brands/shadcn-ui.png",
+        name: "Shadcn/UI",
+    },
+    {
+        url: "/brands/styled-components.png",
+        name: "Styled-components",
+    },
+    {
+        url: "/brands/tailwindcss.png",
+        name: "Tailwindcss",
+    },
+    {
+        url: "/brands/typescript.png",
+        name: "Typescript",
+    },
+    {
+        url: "/brands/vite.png",
+        name: "Vite",
+    },
+]
+
+export const testingTools: Techs[] = [
+    {
+        url: "/brands/jest.png",
+        name: "Jest",
+    },
+    {
+        url: "/brands/react-testing-library.png",
+        name: "React Testing Library",
+    },
+]
+
+export const design: Techs = { url: "/brands/figma.png", name: "Figma", learning: true }
+
+export const database: Techs[] = [
+    {
+        url: "/brands/mysql.png",
+        name: "MySQL",
+    },
+    {
+        url: "/brands/mongodb.png",
+        name: "MongoDB",
+    },
+    {
+        url: "/brands/postgresql.png",
+        learning: true,
+        name: "PostgreSQL",
+    },
+]
+
+export const versionControl: Techs[] = [
+    {
+        url: "/brands/git.png",
+        name: "Git",
+    },
+    {
+        url: "/brands/github.png",
+        name: "GitHub",
+    },
+]
+
+export const backend: Techs[] = [
+    {
+        url: "/brands/express.png",
+        name: "Express",
+    },
+    {
+        url: "/brands/java.png",
+        name: "Java",
+    },
+    {
+        url: "/brands/javascript.png",
+        name: "JavaScript",
+    },
+    {
+        url: "/brands/nodejs.png",
+        name: "Node.js",
+    },
+    {
+        url: "/brands/sequelize.png",
+        name: "Sequelize",
+    },
+    {
+        url: "/brands/typescript.png",
+        name: "TypeScript",
+    }
+]
+
+export const otherApps: Techs[] = [
+    {
+        url: "/brands/discord.png",
+        name: "Discord",
+    },
+    {
+        url: "/brands/chrome.png",
+        name: "Chrome",
+    },
+    {
+        url: "/brands/compass.png",
+        name: "Compass",
+    },
+    {
+        url: "/brands/eclipse.png",
+        name: "Eclipse IDE",
+    },
+    {
+        url: "/brands/intellij-idea.png",
+        name: "IntelliJ IDEA",
     },
 ]
